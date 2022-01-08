8593757203f5546e65e0b7e5f0128f8638d7c275
Bushra <btarbah2000@gmail.com>
2022-01-08 17:37:40 +0200

    here you go

--- tasks.js
+++ tasks.js
@@ -1,34 +1,47 @@
 console.log("Tasks.js is linked, happy coding Hackers ;)");
 //0. create a variable 'myName' give it the value of your name & create an array called 'myFriends' contains 3 names
-
+var myName = "Bushra";
+var myFriends = ["Ranz", "Ray", "Jacob"] ;
 /** Arrays */
 //1. create an array called 'days' that contains the days of the week starting from 'Monday' to 'Sunday'
-
+var days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 //2. create a function called 'getLastElement' that receives an array
 //& return the last element of the array
-
+function getLastElement(){
+  return days[6] ;
+}
 //after that uncomment the next next Line
-//console.log(getLastElement(days)) // Sunday
+console.log(getLastElement(days)) // Sunday
 
 //3. you have array called 'marks' use this array to make the next tasks
 var marks = [5, 9, 20];
 //3.a print the length of the array
-
+marks.length
+console.log(marks.length)
 //3.b print the index of number 23 in the array
-
+console.log(marks.indexOf(23) ) ;
 //3.c add a number 13 to the end of the array
-
+marks.push(13) ;
 //3.d add number 1 to the beginning of the array
-
+marks.unshift(1) ;
 //3.e check if the array contains number 1 & print the result
+console.log(marks.indexOf(1) ) ;
 
 /**objects */
 //4. create an object called 'person' that has these keys(name, age, favorites[], power{speed, strong}) with value of your choice
-
+var person = {
+  name:'Bushra',
+  age: 21,
+  favorites: ['cats, biology, art, funny stuff'],
+  power:{
+    speed: 0.01,
+    strong: 0.0000001,
+  }
+}
 //after that uncomment the next lines
-//console.log(person.name) //person name
-//console.log(person.favorites[0]) //the first favorite thing
-//console.log(person.power.speed) //his speed value
+console.log(person.name) //person name
+console.log(person.favorites[0]) //the first favorite thing
+console.log(person.power.speed) //his speed value
 
 //5. use this object to do the next tasks
 var post = {
@@ -42,12 +55,15 @@ var post = {
 };
 
 //5.a print the value of post's author
-
+console.log(post.author);
 //5.a print the value of post's likes
+console.log(post.likes);
 
 //5.a print the value of post's tags
+console.log(post.tags);
 
 //5.a print the value of post's comments length
+console.log(post.comments.length);
 
 /**Loops */
 var posts = [
@@ -115,5 +131,13 @@ var posts = [
 
 //6. create a loop on the above 'posts' array
 //& print the 'author', 'title', 'content' & 'likes' of each post & print '----' between each post
+for (var i=0; i<posts.length; i++){
+  console.log(posts[i].author)
+  console.log(posts[i].title)
+console.log(posts[i].content)
+console.log(posts[i].likes)
+console.log('____')
+
+}
 
 /**I'm proud of you My Ninja, keep it up :) */
